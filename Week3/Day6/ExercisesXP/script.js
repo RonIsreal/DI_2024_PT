3f5cd3f62555b2e0a5055917fe1cb1fdf91ff0ee
RonIsreal <38158077+RonIsreal@users.noreply.github.com>
2024-07-11 20:02:35 +0300

    Some more

--- Week3/Day6/ExercisesXP/script.js
+++ Week3/Day6/ExercisesXP/script.js
@@ -43,5 +43,132 @@ newPeople.push("Ronny");
 const maryIndex = newPeople.indexOf("Mary");
 console.log(maryIndex);
 
-const peopleCopy = newPeople.slice();
+const newPeopleCopy = newPeople.slice(1, 3);
+console.log(newPeopleCopy);
 
+const fooIndex = newPeopleCopy.indexOf("Foo");
+console.log(fooIndex); //Index return as -1 because this item is not part of the array
+
+const last = newPeople[newPeople.length - 1];
+console.log(last);
+
+
+//PART II
+
+/* Using a loop, iterate through the people array and console.log each person.
+
+Using a loop, iterate through the people array and exit the loop after you console.log “Devon” .
+Hint: take a look at the break statement in the lesson.*/
+
+for (i=0; i< people.length; i++) {
+  console.log(people[i]);
+}
+
+for (i=0;i<people.length;i++) {
+  if (people[i] === 'Devon') {
+    console.log(people[i]);
+    break;
+  }
+}
+
+/*  Exercise 2 : Your Favorite Colors
+Instructions
+Create an array called colors where the value is a list of your five favorite colors.
+Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
+Hint : create an array of suffixes to do the Bonus
+*/
+
+const colors = ['Red', 'Black', 'White', 'Green', 'Yellow'];
+const suffixes = ['1st', '2nd', '3rd', '4th', '5th'];
+
+for (i=0;i<colors.length;i++) {
+  console.log(`My #${i+1} choice is ${colors[i]}.`);
+  console.log(`My ${suffixes[i]} choice is ${colors[i]}.`);
+}
+
+/*  Exercise 3 : Repeat The Question
+Instructions
+Prompt the user for a number.
+Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+While the number is smaller than 10 continue asking the user for a new number.
+Tip : Which while loop is more relevant for this situation?*/
+
+let userInput = prompt("Please enter a number: ");
+console.log(typeof userInput);
+let num = parseInt(userInput); 
+
+while (num < 10) {
+  userInput = parseInt(prompt("Please enter a number:"));
+  num = userInput; // Convert user input to a number
+}
+
+if (num >= 10) {
+  console.log(`You entered ${num}, which is 10 or greater.`);
+} else {
+  console.log("You cancelled or entered a non-numeric value.");
+}
+
+/* Exercise 4 : Building Management
+Instructions:
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
+
+
+Review About Objects
+Copy and paste the above object to your Javascript file.
+
+Console.log the number of floors in the building.
+
+Console.log how many apartments are on the floors 1 and 3.
+
+Console.log the name of the second tenant and the number of rooms he has in his apartment.
+
+Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. If it is, than increase Dan’s rent to 1200.
+*/
+
+const building = {
+  numberOfFloors: 4,
+  numberOfAptByFloor: {
+      firstFloor: 3,
+      secondFloor: 4,
+      thirdFloor: 9,
+      fourthFloor: 2,
+  },
+  nameOfTenants: ["Sarah", "Dan", "David"],
+  numberOfRoomsAndRent:  {
+      sarah: [3, 990],
+      dan:  [4, 1000],
+      david: [1, 500],
+  },
+}
+
+console.log(building['numberOfFloors']);
+console.log(building['numberOfAptByFloor']['firstFloor'], building['numberOfAptByFloor']['thirdFloor']);
+console.log(building['nameOfTenants'][1], building['numberOfRoomsAndRent']['dan'][0]);
+
+const saraRent = building['numberOfRoomsAndRent']['sarah'][1];
+const davidRent = building['numberOfRoomsAndRent']['david'][1];
+let danRent = building['numberOfRoomsAndRent']['dan'][1]; 
+const sumSaraDavidRent = saraRent + davidRent;
+
+if (sumSaraDavidRent > danRent) {
+  building['numberOfRoomsAndRent']['dan'][1] = 1200; 
+  danRent = building['numberOfRoomsAndRent']['dan'][1]; 
+}
+
+console.log(danRent);
