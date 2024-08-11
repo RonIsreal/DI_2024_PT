dc00a578e589e24bcc2ba7480a024e3c10e833db
RonIsreal <38158077+RonIsreal@users.noreply.github.com>
2024-08-11 16:41:48 +0300

    Corrected DailyChallenge

--- Week21/Day5/DailyChallenge/daily_challenge.py
+++ Week21/Day5/DailyChallenge/daily_challenge.py
@@ -8,3 +8,27 @@ Then, the output should be: bag,hello,without,world'''
 my_list = list(input("Please choose some words and separate them with a comma: ").split(","))
 sorted_list = sorted([x for x in my_list])
 print(sorted_list)
+
+'''Challenge 2 : Longest Word
+Instructions
+Write a function that finds the longest word in a sentence. If two or more words are found, return the first longest word.
+Characters such as apostrophe, comma, period count as part of the word (e.g. O’Connor is 8 characters long).
+Examples
+
+longest_word("Margaret's toy is a pretty doll.") ➞ "Margaret's"
+
+longest_word("A thing of beauty is a joy forever.") ➞ "forever."
+
+longest_word("Forgetfulness is by all means powerless!") ➞ "Forgetfulness"
+'''
+
+def longestWord(sent):
+    words = sent.split()
+    longest_word = ""
+    for word in words:
+        if len(word) > len(longest_word):
+            longest_word = word
+    return longest_word
+
+print(longestWord("Forgetfulness is by all means powerless!"))
+        
